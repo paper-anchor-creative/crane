e6a807ce52f24b2f395a66694487581c66a2589d
ariviom <ariviom@gmail.com>
2017-02-04 11:30:40 -0800

    Building menu

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 	<!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'paper-anchor' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'paper-anchor' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</div>
