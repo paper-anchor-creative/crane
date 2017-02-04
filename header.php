d5898d376c698d2b309d8173048168dae59e0094
ariviom <ariviom@gmail.com>
2017-02-03 18:29:23 -0800

    Creating menu styles

--- header.php
+++ header.php
@@ -23,7 +23,8 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'paper-anchor' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+	<header id="site-header" class="site-header" role="banner">
+		<div class="masthead">
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
@@ -38,12 +39,14 @@
 				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php
 			endif; ?>
-		</div><!-- .site-branding -->
+		</div>
+	<!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'paper-anchor' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+	</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
